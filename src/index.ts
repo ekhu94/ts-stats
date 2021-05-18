ae06cc664210bdf4c196db2da3ee10754db48858
Erik Huang <ekhu94@gmail.com>
2021-05-17 17:13:36 -0700

    defines match results enum

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,19 @@ const matches = fs
   .split('\n')
   .map((row: string): string[] => row.split(','));
 
+enum MatchResults {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
 let newCastleWins = 0;
 
 for (let match of matches) {
-  if (match[1] === 'Newcastle' && match[5] === 'H') {
+  if (match[1] === 'Newcastle' && match[5] === MatchResults.HomeWin) {
     newCastleWins++;
   }
-  if (match[2] === 'Newcastle' && match[5] === 'A') {
+  if (match[2] === 'Newcastle' && match[5] === MatchResults.AwayWin) {
     newCastleWins++;
   }
 }
